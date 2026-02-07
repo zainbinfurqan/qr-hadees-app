5990116bb3b8c9dfae78e7f574c7eceb1fe0ec3a
Zain Ahmed <30855000+zainbinfurqan@users.noreply.github.com>
2026-02-08 03:20:41 +0800

    hadithn page UI

--- app/another/page.tsx
+++ app/another/page.tsx
@@ -50,11 +50,56 @@ getClientIP().then(ip => console.log("Client IP:", ip));
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Random Hadees</h1>
-      <p style={{ fontSize: "1.5rem" }}>{hadees?.number}</p>
-      <p style={{ fontSize: "1.2rem" }}>{hadees?.arab}</p>
-      <p style={{ fontSize: "1.2rem" }}>{hadees?.id}</p>
+    hadees && <div className=" max-w-md rounded overflow-hidden shadow-lg mx-auto p-6 mt-10 bg-green-900 text-center text-white">
+      <h1 className="font-bold text-xl mb-2">Hadees</h1>
+      <div className="flex flex-row text-center justify-center gap-4 mb-4 rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black">
+      <p>Arabic Number: {hadees?.hadiths[0].arabicnumber}</p>
+      <p>Hadith Number: {hadees?.hadiths[0].hadithnumber}</p>
+      </div>
+      <p className="rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black" >{hadees?.hadiths[0].text}</p>
+      {hadees?.hadiths[0] && hadees?.hadiths[0].grades?.map((grade, index) => (
+        <div key={index}  className="rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black">
+          <p>
+            {grade.grade} - {grade.name}
+          </p>
+        </div>
+      ))}
+      <div className="flex flex-row rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black text-center justify-center gap-4">
+      <p >book: {hadees?.hadiths[0].reference.book} | hadith: {hadees?.hadiths[0].reference.hadith} | name: {hadees?.metadata.name}</p>
+      </div>
+      {Object.keys(hadees?.metadata.section).map((key) => (
+        <div key={key} className="rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black text-center justify-center gap-4">
+          <p style={{ fontSize: "1.2rem" }}>{hadees?.metadata.section[key as keyof typeof hadees.metadata.section]}</p>
+        </div>
+      ))}
+      {Object.keys(hadees?.metadata.section_detail).map((key) => (
+        <div key={key}>
+        <div className="flex flex-row rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black text-center justify-center gap-4">
+
+        <p>Arabic number first: {hadees?.metadata.section_detail[key as keyof typeof hadees.metadata.section_detail].arabicnumber_first}</p>
+        <p>Arabic number last: {hadees?.metadata.section_detail[key as keyof typeof hadees.metadata.section_detail].arabicnumber_last}</p>
+        </div>
+        <div className="flex flex-row rounded overflow-hidden shadow-lg p-4 mt-4 bg-white text-black text-center justify-center gap-4">
+        
+        <p>Hadith number first: {hadees?.metadata.section_detail[key as keyof typeof hadees.metadata.section_detail].hadithnumber_first}</p>
+        <p>Hadith number last: {hadees?.metadata.section_detail[key as keyof typeof hadees.metadata.section_detail].hadithnumber_last}</p>
+        </div>
+        </div>
+      ))}
+        <div className="text-sm flex flex-row rounded overflow-hidden shadow-lg p-4 mt-4 bg-black text-white text-center justify-center gap-4">
+        <p className="font-bold">On every hadith you read, I will donate 0.10rm to authentic registered charity organization</p>
+        </div>
+        <div className="text-sm flex flex-col rounded overflow-hidden shadow-lg p-4 mt-4 bg-black text-white text-center justify-center gap-4">
+        <p className="font-bold">If you want to donate by youself, Here are some authentic registered charity organizations:</p>
+        <p>1. MATW Project - <a href="https://matwproject.org/" target="_blank" rel="noopener noreferrer">https://matwproject.org/</a></p>
+        {/* <p>2. Mercy Malaysia - https://www.mercy.org.my/</p>
+        <p>3. Yayasan Amal Malaysia - https://yayasanamalmalaysia.org/</p>
+        <p>4. Global Peace Mission (GPM) - https://www.gpm.org.my/</p>
+        <p>5. Humanitarian Care Malaysia (MyCARE) - https://mycare.org.my/</p> */}
+        </div>
+        <div className="flex flex-row rounded overflow-hidden shadow-lg p-4 mt-4 bg-gray-800 text-white text-center justify-center gap-4">
+        <p className="text-sm">Report at zain.ahmed199524@gmail.com, If you found any wrong hadith</p>
+        </div>
     </div>
   );
 }
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,14 @@
 const config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-nunito)', 'sans-serif'],
+      },
+    },
+  },
   plugins: {
     "@tailwindcss/postcss": {},
+    
   },
 };
-
 export default config;
