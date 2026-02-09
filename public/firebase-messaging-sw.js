b3807418b5b182932f07f86355cc8a281ad509be
Zain Ahmed <30855000+zainbinfurqan@users.noreply.github.com>
2026-02-09 16:26:32 +0800

    removed sensetive key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,7 +8,7 @@ firebase.initializeApp({
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: rocess.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 });
 
 const messaging = firebase.messaging();
