6eba24ab62aa6f06b82ca382536365b5af5622db
Zain Ahmed <30855000+zainbinfurqan@users.noreply.github.com>
2026-02-12 00:43:22 +0800

    refactor

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,8 @@
 // importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 // importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
+import { firebaseConfig } from '../lib/firebase-config'
+
 // let messaging;
 // self.addEventListener('message', (event) => {
 //   if (event.data && event.data.type === 'INIT_FIREBASE') {
@@ -49,15 +51,9 @@
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-});
+firebase.initializeApp(firebaseConfig)
 
-const messaging = firebase.messaging();
+ firebase.messaging()
 
 // Background push notification handler
 self.addEventListener('push', (event) => {
